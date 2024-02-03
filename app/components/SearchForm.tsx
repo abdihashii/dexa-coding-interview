57c306b127254db6e8bcc5a663791588e0afb04f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-03 14:21:39 -0600

    Remove the input key and just reset the search query on submit

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Form, useNavigation } from '@remix-run/react';
-import { v4 as uuidv4 } from 'uuid';
 
 const SearchForm = ({ prevContext }: { prevContext: string }) => {
   const navigation = useNavigation();
@@ -8,25 +7,15 @@ const SearchForm = ({ prevContext }: { prevContext: string }) => {
   // State that will hold the search query for the form input
   const [searchQuery, setSearchQuery] = useState('');
 
-  const [inputKey, setInputKey] = useState(`input-key`);
-
-  // Reset the forms search input
-  useEffect(() => {
-    if (navigation.state === 'idle' && inputKey !== 'input-key') {
-      setInputKey('input-key'); // Reset the key to force-clear the input field
-    }
-  }, [navigation.state, inputKey]);
-
   return (
     <Form
       method="get"
       className="flex flex-col gap-4"
       onSubmit={() => {
-        setInputKey(`input-key-${uuidv4()}`);
+        setSearchQuery('');
       }}
     >
       <input
-        key={inputKey}
         className="border border-gray-300 rounded-md p-2"
         type="search"
         name="q"
